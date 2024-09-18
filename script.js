cd0cd5c08e39e711d0d25acb9e32184472d254e9
JustNormalProgrammer <testkonto1290@gmail.com>
2024-09-18 23:14:55 +0200

    Fix a bug with "=" and "." buttons.
    Now user connot add a "." operator, when a number
    evaluated already has one. User can change the
    number after evaluation and immediately add an
    operator after it to calculate next value, without
    having to reset the display using "C" operator.
    After entering any number following with a "."
    operator, program will now automaticaly add a "0"
    to make it clearer to the user what number he is
    currently working on.

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const numbers = "0123456789";
 const numberButtons = [];
 const operatorButtons = [];
 Array.from(buttons).forEach(button => {
-    if(numbers.includes(button.textContent)){
+    if (numbers.includes(button.textContent)) {
         numberButtons.push(button);
     } else {
         operatorButtons.push(button);
-    }    
+    }
 });
 
 numberButtons.forEach(numBtn => {
@@ -19,95 +19,108 @@ operatorButtons.forEach(opBtn => {
 })
 
 let isOperatorLast = false;
-let isCommaLast = false; 
+let isCommaLast = false;
+let isCommaInNumber = false;
 let idxOfOperatorInEquation = 0;
 let num1 = '';
 let num2 = '';
 
-function displayOperator(opBtn){
+function displayOperator(opBtn) {
     let value = opBtn.textContent;
-    switch(value){
-        case "C": 
+    switch (value) {
+        case "C":
             result.textContent = '';
             isCommaLast = false;
             isOperatorLast = false;
+            isCommaInNumber = false;
             idxOfOperatorInEquation = 0;
-            break; 
+            break;
         case "⌫":
-            if(isCommaLast) isCommaLast = false;
-            if(isOperatorLast) {
+            if (isCommaLast) isCommaLast = false;
+            if (isOperatorLast) {
                 isOperatorLast = false;
                 idxOfOperatorInEquation = 0;
             }
-            result.textContent = result.textContent.slice(0,-1);
+            result.textContent = result.textContent.slice(0, -1);
             break;
         case ".":
-            if(isCommaLast) break;
-            if(isOperatorLast) break;
+            if ((isCommaLast || isCommaInNumber || isOperatorLast) && num2!==null) break;
             result.textContent += ".";
             isCommaLast = true;
+            isCommaInNumber = true;
             break;
         default:
-            if(isOperatorLast && value) {
-                result.textContent = result.textContent.slice(0,-1) + value;
+            console.log(idxOfOperatorInEquation)
+            if (isOperatorLast && value !== "=") {
+                result.textContent = result.textContent.slice(0, -1) + value;
                 break;
             }
-            if(isCommaLast) break;
-            if(idxOfOperatorInEquation) {
-                num2 = parseFloat(result.textContent.slice(idxOfOperatorInEquation+1));
+            if (isCommaLast) result.textContent += "0";
+            if (idxOfOperatorInEquation) {
+                num2 = parseFloat(result.textContent.slice(idxOfOperatorInEquation + 1));
                 getResult(result.textContent.charAt(idxOfOperatorInEquation));
-                if(value === "=") {
+                if (value === "=") {
                     result.textContent = num1;
+                    isCommaLast = false;
+                    isOperatorLast = false;
+                    idxOfOperatorInEquation = 0;
                     break;
                 }
+                if(value !== "="){
                 result.textContent = num1 + value;
                 isOperatorLast = true;
-                idxOfOperatorInEquation = result.textContent.length-1;
-                console.log(idxOfOperatorInEquation+1)
+                idxOfOperatorInEquation = result.textContent.length - 1;
                 break;
-            }else {
+                }
+            } else {
                 num1 = parseFloat(result.textContent);
             }
-            idxOfOperatorInEquation = result.textContent.length;
-            result.textContent += value;
-            isOperatorLast = true;
-            break;
+            if(value === "=" && idxOfOperatorInEquation === 0) break;
+                idxOfOperatorInEquation = result.textContent.length;
+                result.textContent += value;
+                isCommaInNumber = false;
+                isOperatorLast = true;
+                break;
     }
 }
 
 
-function displayNumber(nodeBtn){
+function displayNumber(nodeBtn) {
+    // if (num2 === null) {
+    //     result.textContent = '';
+    //     num2 = '';
+    //     num1 = '';
+    //     isCommaLast = false;
+    //     isOperatorLast = false;
+    //     isCommaInNumber = false;
+    //     idxOfOperatorInEquation = 0;
+    // }
     let value = nodeBtn.textContent;
-    isOperatorLast = false;
     result.textContent += value;
+    isOperatorLast = false;
     isCommaLast = false;
 }
-function getResult(operator){
-    switch(operator){
+function getResult(operator) {
+    switch (operator) {
         case "÷":
             num1 = num1 / num2;
-            num2 = '';
+            num2 = null;
             break;
         case "×":
             num1 = num1 * num2;
-            num2 = '';
+            num2 = null;
             break;
         case "−":
             num1 = num1 - num2;
-            num2 = '';
+            num2 = null;
             break;
         case "+":
             num1 = num1 + num2;
-            num2 = '';
+            num2 = null;
             break;
         case "%":
             num1 = num1 % num2;
-            num2 = '';
+            num2 = null;
             break;
-    }
-}
-function operate(num1, num2, operator){
-    switch(operator){
-        case "":break;
     }
 }
